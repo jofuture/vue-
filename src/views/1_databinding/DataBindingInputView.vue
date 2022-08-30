b97a84cf09c4e7a790cd1fbaa7246afa3b70d80b
jofuture <721mm@naver.com>
2022-08-31 01:14:46 +0900

    첫커밋

---
+++ src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="test" v-model="userId" />
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'mirae.jo'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Mirae Jo',
+      message: 'Welcome 미래'
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
